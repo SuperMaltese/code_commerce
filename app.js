ccd025ec744ce96ce218c269e71c62d94f5d880b
mountaindrake <kevinyanggao@gmail.com>
2019-10-26 18:18:40 -0700

    lost a file, committing to recover

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
+var path = require("path");
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(
+  "/",
+  express.static(path.join(__dirname, "node_modules/codemirror"))
+);
 
 app.get('/', function (req, res) {
   res.sendFile('index.html', { root: __dirname });
